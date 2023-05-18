577351ee566882c02548e23fb0189cda4285367c
Daniel Rees <daniel_james_rees@hotmail.com>
2023-05-17 20:56:08 -0400

    fix: add missing links to actually access all pages

--- src/routes/characters/+page.svelte
+++ src/routes/characters/+page.svelte
@@ -8,23 +8,26 @@
 	const { form: form2, enhance: enhance2, allErrors } = superForm(data.importForm);
 </script>
 
-<form method="post" action="?/upload" enctype="multipart/form-data" use:enhance>
-	<label>
-		<span>Upload JSON</span>
-		<input class="input" type="file" name="file" id="file" />
-	</label>
-	<button class="btn variant-filled-primary" type="submit">Submit</button>
-</form>
-<p>https://pathbuilder2e.com/json.php?id=140976</p>
-<ul>
-	{#each $allErrors as err}
-		<li>{err.message} - {err.path}</li>
-	{/each}
-</ul>
-<form method="post" action="?/importCharacter" use:enhance2>
-	<label>
-		<span>Pathbuilder URL</span>
-		<input class="input" type="number" name="id" id="id" bind:value={$form2.id} />
-	</label>
-	<button class="btn variant-filled-primary" type="submit">Submit</button>
-</form>
+<div class="py-5">
+	<form method="post" action="?/upload" enctype="multipart/form-data" use:enhance>
+		<label class="label">
+			<span>Upload JSON</span>
+			<input class="input" type="file" name="file" id="file" />
+		</label>
+		<button class="btn variant-filled-primary" type="submit">Submit</button>
+	</form>
+</div>
+<div class="py-5">
+	<ul>
+		{#each $allErrors as err}
+			<li>{err.message} - {err.path}</li>
+		{/each}
+	</ul>
+	<form method="post" action="?/importCharacter" use:enhance2>
+		<label class="label">
+			<span>Pathbuilder JSON ID</span>
+			<input class="input" type="number" name="id" id="id" bind:value={$form2.id} />
+		</label>
+		<button class="btn variant-filled-primary" type="submit">Submit</button>
+	</form>
+</div>
---
+++ src/routes/encounters/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div>
+	<h1>Encounters</h1>
+
+	<ul>
+		<li><a href="/encounters/create">Create</a></li>
+	</ul>
+</div>
